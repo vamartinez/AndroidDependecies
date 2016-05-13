5f958d7d17bb736ec5958b062aafebe673ef232f
Victor Martinez <Victor@iMac-de-Victor-2.local>
2016-05-13 11:15:34 -0500

    complete the test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsynckTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsynckTest.java
@@ -1,20 +1,34 @@
 package com.udacity.gradle.builditbigger;
 
+import android.test.AndroidTestCase;
+
 import junit.framework.TestCase;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Created by vic on 13/05/2016.
  */
-public class AsynckTest extends TestCase {
+public class AsynckTest extends AndroidTestCase {
 
-  @Test
+    @Test
     public void testSomething() throws Throwable {
-        Assert.assertTrue(1 + 1 == 2);
+        Assert.assertTrue(1 + 2 == 3);
     }
-    public void testCallBack() throws Throwable {
 
+    @Test
+    public void testCallBack() throws Throwable {
+        final CountDownLatch signal = new CountDownLatch(1);
+        new EndpointsAsyncTask(new EndpointsAsyncTask.AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+                Assert.assertTrue(output != null && output.length() > 0);
+                signal.countDown();
+            }
+        }).execute(getContext());
+        signal.await();
     }
 }
